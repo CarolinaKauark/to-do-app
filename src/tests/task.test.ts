e07e3ce3db2b0e904ce3e379957f83a27f9e2466
Ckauark <ckauark@gmail.com>
2023-01-30 16:32:18 -0300

    feat: route post /task + test

--- src/tests/task.test.ts
+++ src/tests/task.test.ts
@@ -19,7 +19,7 @@ const { app } = new App();
 
 describe('Testing the task route', () => {
 
-  describe('Test "/tasks" route', () => {
+  describe('Test get "/task" route', () => {
 
     let chaiHttpResponse: Response;
   
@@ -38,161 +38,84 @@ describe('Testing the task route', () => {
          .request(app)
          .get('/task')
          .set('Authorization', tokenMock);
-         
+
       expect(chaiHttpResponse.status).to.equal(200);
       expect(chaiHttpResponse.body).to.deep.equal(tasks);
+    });  
+  });
+
+  describe('Test post "/task" route', () => {
+
+    afterEach(()=>{
+      (Task.create as sinon.SinonStub).restore();
+      (jwt.verify as sinon.SinonStub).restore();
+    })
+
+    it('tests if it is possible to add tasks in the post /task route', async () => {
+      sinon
+          .stub(Task, "create")
+          .resolves(tasks[0] as ITask | any);
+      sinon
+          .stub(jwt, 'verify').resolves({ id: 2 });
+      const chaiHttpResponse = await chai
+         .request(app)
+         .post('/task')
+         .set('Authorization', tokenMock)
+         .send({
+          description: "new task",
+          startTime: "2015-05-28T16:00:00.000Z",
+          endTime: "2015-05-29T00:00:00.000Z",
+          isHighPriority: true,
+          inProgress: true
+        })
+    
+      expect(chaiHttpResponse.status).to.equal(201);
+      expect(chaiHttpResponse.body).to.deep.equal(tasks[0]);
     });
-  //   it('testa se os matches apenas os true na rota get /matches é retornado', async () => {
-  //     sinon
-  //         .stub(Match, "findAll")
-  //         .resolves(matchTrue as IMatch[] | any);
-  //     const chaiHttpResponse = await chai
-  //        .request(app)
-  //        .get('/matches?inProgress=true')
-  
-  //     expect(chaiHttpResponse.status).to.equal(200);
-  //     expect(chaiHttpResponse.body).to.deep.equal(matchTrue);
-  //   });
-  
-  //   it('testa se os matches apenas os false na rota get /matches é retornado', async () => {
-  //     sinon
-  //         .stub(Match, "findAll")
-  //         .resolves(matchFalse as IMatch[] | any);
-  //     const chaiHttpResponse = await chai
-  //        .request(app)
-  //        .get('/matches?inProgress=false')
-  
-  //     expect(chaiHttpResponse.status).to.equal(200);
-  //     expect(chaiHttpResponse.body).to.deep.equal(matchFalse);
-  //   });
-  
-  // });
 
-  // describe('Testa a rota post /matches', () => {
-
-  //   afterEach(()=>{
-  //     (Match.create as sinon.SinonStub).restore();
-  //     (jwt.verify as sinon.SinonStub).restore();
-  //   })
-
-  //   it('testa se é possível adicionar matches na rota post /matches', async () => {
-  //     sinon
-  //         .stub(Match, "create")
-  //         .resolves(insertMatch as IMatch[] | any);
-  //     sinon
-  //         .stub(jwt, 'verify').resolves({ id: 1 });
-  //     const chaiHttpResponse = await chai
-  //        .request(app)
-  //        .post('/matches')
-  //        .set('Authorization', tokenMock)
-  //        .send({
-  //         homeTeam: 16,
-  //         awayTeam: 8, 
-  //         homeTeamGoals: 2,
-  //         awayTeamGoals: 2,
-  //       })
+    it('tests whether it is not possible to add tasks in the post /task route without the startTime', async () => {
+      sinon
+          .stub(Task, "create")
+          .resolves(tasks[0] as ITask | any);
+      sinon
+          .stub(jwt, 'verify').resolves({ id: 2 });
+      const chaiHttpResponse = await chai
+         .request(app)
+         .post('/task')
+         .set('Authorization', tokenMock)
+         .send({
+          description: "new task",
+          endTime: "2015-05-29T00:00:00.000Z",
+          isHighPriority: true,
+          inProgress: true
+        })
     
-  //     expect(chaiHttpResponse.status).to.equal(201);
-  //     expect(chaiHttpResponse.body).to.deep.equal(insertMatch);
-  //   });
+      expect(chaiHttpResponse.status).to.equal(400);
+      expect(chaiHttpResponse.body).to.deep.equal({
+        message: 'All fields must be filled',
+      });
+    });
 
-  //   it('testa se é não possível adicionar matches com os dois times tendo o mesmo id', async () => {
-  //     sinon
-  //         .stub(Match, "create")
-  //         .resolves(insertMatch as IMatch[] | any);
-  //     sinon
-  //         .stub(jwt, 'verify').resolves({ id: 1 });
-  //     const chaiHttpResponse = await chai
-  //       .request(app)
-  //       .post('/matches')
-  //       .set('Authorization', tokenMock)
-  //       .send({
-  //         homeTeam: 8,
-  //         awayTeam: 8, 
-  //         homeTeamGoals: 2,
-  //         awayTeamGoals: 2,
-  //       })
+    it('test if the error generator is called without the token', async () => {
+      sinon
+          .stub(Task, "create")
+          .resolves(tasks[0] as ITask | any);
+      sinon
+          .stub(jwt, 'verify').resolves({ id: 2 });
+      const chaiHttpResponse = await chai
+         .request(app)
+         .post('/task')
+         .set('Authorization', '')
+         .send({
+          description: "new task",
+          startTime: "2015-05-28T16:00:00.000Z",
+          endTime: "2015-05-29T00:00:00.000Z",
+          isHighPriority: true,
+          inProgress: true
+        })
     
-  //     expect(chaiHttpResponse.status).to.equal(422);
-  //     expect(chaiHttpResponse.body).to.deep.equal({message: 'It is not possible to create a match with two equal teams'});
-  //   });
-
-  //   it('testa ao acontecer um erro o throw new error do auth middleware é chamado', async () => {
-  //     sinon
-  //         .stub(Match, "create")
-  //         .resolves(insertMatch as IMatch[] | any);
-  //     sinon
-  //         .stub(jwt, 'verify').throws();
-  //     const chaiHttpResponse = await chai
-  //       .request(app)
-  //       .post('/matches')
-  //       .set('Authorization', tokenMock)
-  //       .send({
-  //         homeTeam: 8,
-  //         awayTeam: 10, 
-  //         homeTeamGoals: 2,
-  //         awayTeamGoals: 2,
-  //       })
-    
-  //     expect(chaiHttpResponse.status).to.equal(401);
-  //     expect(chaiHttpResponse.body).to.deep.equal({message: 'Token must be a valid token'});
-  //   });
-
-  //   it('testa se o errorGenerator é chamado sem o token', async () => {
-  //     sinon
-  //         .stub(Match, "create")
-  //         .resolves(insertMatch as IMatch[] | any);
-  //     sinon
-  //         .stub(jwt, 'verify').throws();
-  //     const chaiHttpResponse = await chai
-  //       .request(app)
-  //       .post('/matches')
-  //       .set('Authorization', '')
-  //       .send({
-  //         homeTeam: 8,
-  //         awayTeam: 10, 
-  //         homeTeamGoals: 2,
-  //         awayTeamGoals: 2,
-  //       })
-    
-  //     expect(chaiHttpResponse.status).to.equal(401);
-  //     expect(chaiHttpResponse.body).to.deep.equal({message: 'Token not found'});
-  //   });
-  // });
-
-  // describe('testa a rota patch /matches', () => {
-
-  //   afterEach(() => {
-  //     (Match.update as sinon.SinonStub).restore();
-  //   })
-  //   it('testa se o update na rota patch /matches/1/finish é feito', async () => {
-  //     sinon
-  //         .stub(Match, "update")
-  //         .resolves([1] as any);
-  //     const chaiHttpResponse = await chai
-  //        .request(app)
-  //        .patch('/matches/1/finish')
-  //        .set('Authorization', tokenMock)
-  
-  //     expect(chaiHttpResponse.status).to.equal(200);
-  //     expect(chaiHttpResponse.body).to.deep.equal({ message: 'Finished' });
-  //   });
-
-  //   it('testa se o update na rota patch /matches/1 é feito', async () => {
-  //     sinon
-  //         .stub(Match, "update")
-  //         .resolves([1] as any);
-  //     const chaiHttpResponse = await chai
-  //        .request(app)
-  //        .patch('/matches/1')
-  //        .set('Authorization', tokenMock)
-  //        .send({
-  //         homeTeamGoals: 3,
-  //         awayTeamGoals: 1
-  //        })
-  
-  //     expect(chaiHttpResponse.status).to.equal(200);
-  //     expect(chaiHttpResponse.body).to.deep.equal({ message: 'Match is updated' });
-  //   });    
+      expect(chaiHttpResponse.status).to.equal(401);
+      expect(chaiHttpResponse.body).to.deep.equal({message: 'Token not found'});
+    });
   });
 });
